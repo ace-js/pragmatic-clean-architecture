519b1dc43310fa13a42cc27f244e363cf9963d3e
acejs <bertieaux.a@gmail.com>
2023-08-16 14:34:53 +0200

    WIP Application Layer

---
+++ Bookify/src/Bookify.Domain/Bookings/IBookingRepository.cs
@@ -0,0 +1,12 @@
+using Bookify.Domain.Apartments;
+
+namespace Bookify.Domain.Bookings;
+
+public interface IBookingRepository
+{
+    Task<Booking> GetByIdAsync(Guid bookingId, CancellationToken cancellationToken);
+
+    Task<bool> IsOverlappingAsync(Apartment apartment, DateRange duration, CancellationToken cancellationToken);
+
+    void Add(Booking booking);
+}
---
+++ Bookify/src/Bookify.Domain/Users/UserErrors.cs
@@ -0,0 +1,8 @@
+using Bookify.Domain.Abstractions;
+
+namespace Bookify.Domain.Users;
+
+public static class UserErrors
+{
+    public static Error NotFound = new("User.NotFound", "The user with the specified identifier was not found");
+}
